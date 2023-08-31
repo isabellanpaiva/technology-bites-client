3e672777af7a81a73e8f2dc1a99c7d1d7807450e
Arian <arianraiesteban_20@hotmail.com>
2023-08-31 13:41:22 +0200

    version 2.2 bite services

---
+++ src/services/bite.services.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+class BiteServices {
+	constructor() {
+		this.api = axios.create({
+			baseURL: `${import.meta.env.VITE_API_URL}/bite`,
+		})
+		this.api.interceptors.request.use(config => {
+			const storedToken = localStorage.getItem('authToken')
+			if (storedToken) {
+				config.headers = { Authorization: `Bearer ${storedToken}` }
+			}
+			return config
+		})
+	}
+
+	createOne(biteData) {
+		return this.api.post('/create', biteData)
+	}
+
+	createMany(bitesJson) {
+		return this.api.post('/createMany', bitesJson)
+	}
+
+	getOneRandom() {
+		return this.api.post('/getOneRandom')
+	}
+}
+
+const biteServices = new BiteServices()
+
+export default biteServices
